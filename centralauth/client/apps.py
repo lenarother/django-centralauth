f0f90cc8c452fe7d8183b823446059f23f55da0e
Stephan Jaekel <steph@rdev.info>
2018-11-16 11:18:41 +0100

    Enforce installation of middleware in centralauth client.

--- centralauth/client/apps.py
+++ centralauth/client/apps.py
@@ -1,12 +1,21 @@
 from django.apps import AppConfig
 from django.conf import settings
 from django.contrib import admin
+from django.core.exceptions import ImproperlyConfigured
 
 
 class ClientConfig(AppConfig):
     name = 'centralauth.client'
 
     def ready(self):
+        if 'centralauth.client.middleware.CentralAuthSyncMiddleware' not in getattr(
+            settings, 'MIDDLEWARE', getattr(settings, 'MIDDLEWARE_CLASSES', ())
+        ):
+            raise ImproperlyConfigured(
+                'You cannot use CentralAuth without installing '
+                'centralauth.client.middleware.CentralAuthSyncMiddleware'
+            )
+
         login_template = getattr(settings, 'CENTRALAUTH_CUSTOM_LOGIN_TEMPLATE', False)
         if login_template:
             admin.site.login_template = (
