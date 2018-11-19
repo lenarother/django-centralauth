62e006becaf995400f61a741680af2290bd0a622
Magdalena Rother <magdalena.rother@moccu.com>
2018-11-19 11:34:52 +0100

    Test client constants

---
+++ tests/client/test_constants.py
@@ -0,0 +1,17 @@
+import pytest
+from django.core.exceptions import ImproperlyConfigured
+
+from centralauth.client.constants import provider_url
+
+
+@pytest.mark.django_db
+class TestConstants:
+
+    def test_provider_url(self, settings):
+        settings.CENTRALAUTH_PROVIDER_URL = 'http://provider.com/'
+        assert provider_url() == 'http://provider.com'
+
+    def test_no_provider_url(self, settings):
+        settings.CENTRALAUTH_PROVIDER_URL = None
+        with pytest.raises(ImproperlyConfigured):
+            provider_url()
